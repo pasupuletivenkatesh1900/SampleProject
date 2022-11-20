8e81aaea66136c4f808be0ca95581cd4cd3ee318
VenkateshProjects <nanivenkatesh17@gmail.com>
2022-11-20 22:05:03 +0530

    laptop commit

--- src/test/java/Sampleproject/Sampleproject/Text.java
+++ src/test/java/Sampleproject/Sampleproject/Text.java
@@ -4,7 +4,7 @@ public class Text {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("SomeThing");
+		System.out.println("SomeThing newly added");
 	}
 
 }
