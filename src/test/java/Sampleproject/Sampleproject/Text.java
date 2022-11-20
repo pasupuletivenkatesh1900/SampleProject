04b127a175a9df3fbd2b9988241de4881788e9db
P V A V RAMANA VENKATESH <63536144+pasupuletivenkatesh1900@users.noreply.github.com>
2022-11-20 22:26:53 +0530

    this is my turn in deskop

--- src/test/java/Sampleproject/Sampleproject/Text.java
+++ src/test/java/Sampleproject/Sampleproject/Text.java
@@ -4,9 +4,8 @@ public class Text {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("SomeThing newly added");
-		System.out.println("testing desktop");
-		System.out.println("testing desktop2");
+	
+		System.out.println("magic");
 
 	}
 
